5e92da8d477251205ec4b5a858e79f338c1b3e1c
Jonathan Depaepe <jonathan.depaepe@student.howest.be>
2022-12-01 15:54:16 +0100

    tournament club page

--- pages/club.js
+++ pages/club.js
@@ -76,10 +76,9 @@ export default function Club() {
                     </div>
                 </div>
             </main>
-            <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" rel="stylesheet"
-                  integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
-                  crossOrigin="anonymous"></script>
-            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+
+            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
         </>
     )
 }
